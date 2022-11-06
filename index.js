19cb65b79479b16791eb39d77fc40d8af2f8f2e1
s1xtyten <102329248+s1xtyten@users.noreply.github.com>
2022-11-06 23:09:04 +0100

    work on assignment

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-const grid = document.querySelector('#container');
-let tileContent = 1;
+const grid = document.querySelector('#grid');
 
 
 for (let i = 0; i < 256; i++) {
   const tile = document.createElement("div");
   tile.classList.add("tile");
-  tile.innerHTML = tileContent;
   grid.appendChild(tile);
-  tileContent += 1;
 };
 
 const tile = document.getElementsByClassName('tile');
